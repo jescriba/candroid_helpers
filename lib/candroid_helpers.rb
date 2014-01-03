187f1557907133ffdb7d79401eef1c2fc711b6e2
jescriba <escribirajoshua@gmail.com>
2014-01-03 14:58:12 -0800

    Making pre gem version.

--- lib/candroid_helpers.rb
+++ lib/candroid_helpers.rb
@@ -3,24 +3,33 @@ require "calabash-android/abase"
 
 module CandroidHelpers
   
-  def until_element_exists element, action, timeout = 15
-    '''Performs a lambda action until the element appears.'''
+  def until_element_exists opts = {}
+    '''Performs a lambda action until the element appears.
+    The default action is to do nothing.'''
+    raise "No element given." if opts[:element].nil?
+    timeout = opts[:timeout] || 10
+    action = opts[:action] || lambda { ; }
     wait_poll until_exists: element, timeout: timeout do
       action.call
     end
   end
 
-  def once_element_exists element, action = lambda { touch element }, timeout = 15
+  def once_element_exists opts = {}
     '''Performs a lambda action once the element exists.
     The default behavior is to touch the specified element.'''
+    raise "No element given." if opts[:element].nil?
+    timeout = opts[:timeout] || 10
+    action = opts[:action] || lambda { touch element }
     wait_for_elements_exist [element], timeout: timeout
     action.call
   end
 
-  def multiple_traits arr = ["*"], timeout = 15
+  def multiple_traits opts = {}
     '''Pass an array of query elements. Determines the
     correct trait for page objects that can have multiple 
     acceptable traits.'''
+    timeout = opts[:timeout] || 10
+    traits = opts[:traits] || ["*"]
     trait = ''
     action = lambda do 
       arr.each do |element|
